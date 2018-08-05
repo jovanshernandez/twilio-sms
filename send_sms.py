1475c00107ea279bbf48b02792d383c64ce10289
Jovan Hernandez <jovanshernandez@gmail.com>
2018-08-05 15:54:36 -0400

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -4,14 +4,15 @@ from twilio.rest import Client
 
 
 # Your account SID and Auth Token from twilio.com/console
-account_sid = 'AC202b5d1de5e90340b6b0c7dc628d595b'
-auth_token = '1f53509d240fc4d95fa85f9767e78796'
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token_here'
 client =  Client(account_sid, auth_token)
 
+# Change the body, from, and to fields to whatever you want or need.
 message = client.messages.create(
-				body='Do you want to take the red pill, or blue pill?',
-				from_='[+][1][6105107306]',
-				to='[+][1][6105976107]',
+				body='Hello, world!',
+				from_='[+][country_code][your_twilio_number_here]',
+				to='[+][country_code][target_number]',
 			)
 
 print(message.sid)
